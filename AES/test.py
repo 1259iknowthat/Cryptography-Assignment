dbd0a47d46caeab681d0c46bff048ef269e7143b
1259iknowthat <huaduy1259@gmail.com>
2023-04-22 21:17:58 +0700

    remove duplicate prob

--- AES/test.py
+++
@@ -1,8 +0,0 @@
-import os
-from Crypto.Util.Padding import pad
-from datetime import datetime, timedelta
-flag = 'crypto{testing_test}'.encode()
-expires_at = (datetime.today() + timedelta(days=1))
-cookie = f"admin=False;expiry={expires_at}".encode()
-padded = pad(cookie, 16)
-print(padded[:16])
