7454a150937fc8408d188271e8ad0ac4cb01e34a
1259iknowthat <huaduy1259@gmail.com>
2023-04-23 22:36:59 +0700

    update solution

--- Symetric_Cryptography/Structure_of_AES/solve.py
+++ Symetric_Cryptography/Structure_of_AES/solve.py
@@ -1,17 +1,11 @@
-def bytes2matrix(text):
-    """ Converts a 16-byte array into a 4x4 matrix.  """
-    return [list(text[i:i+4]) for i in range(0, len(text), 4)]
-
 def matrix2bytes(matrix):
     """ Converts a 4x4 matrix into a 16-byte array.  """
-    s=""
+    s = ""
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            s+=chr(matrix[i][j])
+            s += chr(matrix[i][j])
     return s
-    """
-    return bytes(sum(matrix, [])) 
-    """
+
 matrix = [
     [99, 114, 121, 112],
     [116, 111, 123, 105],
@@ -19,10 +13,5 @@ matrix = [
     [114, 105, 120, 125],
 ]
 
-
-flag="abcdefgh"
-ct=bytes2matrix(flag)
-print(ct)
-ms=matrix2bytes(matrix)
-print(ms)
-
+# print(matrix2bytes(matrix))
+print(matrix2bytes(matrix))
