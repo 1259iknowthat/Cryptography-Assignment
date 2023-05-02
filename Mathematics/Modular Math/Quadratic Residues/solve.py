83cc74d7277faeed8df7ca0f0ac627a941e8f675
Ngô Vinh Huy <31349426+robbert1978@users.noreply.github.com>
2023-05-02 21:59:52 +0700

    better sol ????

--- Mathematics/Modular Math/Quadratic Residues/solve.py
+++ Mathematics/Modular Math/Quadratic Residues/solve.py
@@ -3,7 +3,7 @@ ints = [14, 6, 11]
 
 for _ in ints:
     print(_,end=':\n')
-    for i in range(100):
+    for i in range(p):
         t=(_+p*i)**0.5
         if(int(t)==t):
             print(t,end=' ')
