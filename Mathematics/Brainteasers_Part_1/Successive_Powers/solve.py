e311422b21d3aa7592582fa2d22147ac977a15d4
tranminhprvt01 <tranminhprvt01@gmail.com>
2023-05-05 12:15:02 +0700

    edit

--- Mathematics/Brainteasers_Part_1/Successive_Powers/solve.py
+++ Mathematics/Brainteasers_Part_1/Successive_Powers/solve.py
@@ -1,21 +1,22 @@
-"""
-i=1
-while (i < 3):
-	n=588**(2*i)-665
-	k=1
-	print("cc")
-	print(n)
-	while len(str(n//k)) > 3:
-		k+=1
-	print(k)
-	print(n/k)
-	while n//k >= 851:
-		if n%k != 0 : continue
-		print(i,n,k,n/k)
-		k+=1
-	i+=1
-"""
+#This code is contributed by tranminhprvt01
+from math import sqrt
+def factors(n):    
+    j = 2
+    while n > 1:
+        for i in range(j, int(sqrt(n+0.05)) + 1):
+            if n % i == 0:
+                n //= i ; j = i
+                yield i
+                break
+        else:
+            if n > 1:
+                yield n; break
 
-for p in range(1,114*2091000):
-	if 588**2 % p == 665:
-		print(p)
+
+                
+x = 851 - 642
+
+x = 588*209 - 665 #this x will equal k*p so now we factorize this to find p which is 3-digits 
+
+ls = [i for i in factors(x)]
+print(ls)
